e28088089771d1f637d09bc2d464d82cbebe7ab3
Luca Casarini <llucc@amazon.com>
2024-05-01 14:47:30 +0200

    Finalized RDS inventory and filters on Inventory tab

--- athena_queries/views/configuration_management/config_inventory_all.sql
+++ athena_queries/views/configuration_management/config_inventory_all.sql
@@ -1,12 +1,37 @@
 CREATE OR REPLACE VIEW "config_inventory_all" AS 
 SELECT DISTINCT
-  "accountId" "AccountId"
-, "region" "Region"
-, "configurationItem"."resourceid" "ResourceId"
-, "configurationItem"."resourcename" "ResourceName"
-, "configurationItem"."resourcetype" "ResourceType"
-, "configurationItem"."availabilityzone" "AvailabiltyZone"
-, "configurationItem"."resourcecreationtime" "CreationTime"
+  a."AccountId"
+, a."Region"
+, a."Date"
+, a."Arn"
+, a."TAG1"
+, a."TAG2"
+, a."TAG3"
+, a."TAG4"
+, a."AllTags"
+, a."LastConfigSnapshot"
+, a."ResourceType"
 FROM
-  ("cid_config_athena_database"."aws_config_configuration_snapshots"
-CROSS JOIN UNNEST("configurationitems") t (configurationItem))
+  (
+   SELECT *
+   FROM
+     (
+      SELECT DISTINCT
+        "configurationItem"."awsAccountId" "AccountId"
+      , "configurationItem"."awsregion" "Region"
+      , CAST(date_parse("dt", '%Y-%m-%d') AS "Date") "Date"
+      , "configurationItem"."tags"['CostCenter'] "TAG1"
+      , "configurationItem"."tags"['Application'] "TAG2"
+      , "configurationItem"."tags"['Environment'] "TAG3"
+      , "configurationItem"."tags"['Owner'] "TAG4"
+      , "configurationItem"."tags" "AllTags"
+      , "configurationItem"."configurationitemcapturetime" "LastConfigSnapshot"
+      , "configurationItem"."resourcetype" "ResourceType"
+      , "configurationItem"."arn" "Arn"
+      , ROW_NUMBER() OVER (PARTITION BY "configurationItem"."arn", YEAR(CAST(date_parse("dt", '%Y-%m-%d') AS "Date")), MONTH(CAST(date_parse("dt", '%Y-%m-%d') AS "Date")) ORDER BY CAST(date_parse("dt", '%Y-%m-%d') AS "Date") DESC) Rowrank
+      FROM
+        (cid_crcd_config
+      CROSS JOIN UNNEST("configurationitems") t (configurationItem))
+   )  sub
+   WHERE (Rowrank = 1)
+)  a
