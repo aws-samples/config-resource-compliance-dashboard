acec44f70af5d0dda4748e8bfa9967718a779e02
Iakov Gan <iakov@amazon.com>
2025-01-06 11:25:25 +0100

    fix release script

--- release.sh
+++ release.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws s3 cp  ./dashboard_template/cid-crcd.yaml  s3://aws-managed-cost-intelligence-dashboards/cfn/cid-crcd.yaml
+aws s3 cp  ./cloudformation/cid-crcd-resources.yaml  s3://aws-managed-cost-intelligence-dashboards/cfn/cid-crcd-resources.yaml
