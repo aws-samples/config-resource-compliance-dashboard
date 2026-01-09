36a6d781a9c5e6026987350306c53845729af00b
Luca Casarini <llucc@amazon.com>
2026-01-09 16:33:13 +0100

    development file and release script

--- release.sh
+++ release.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-aws s3 cp  ./cloudformation/cid-crcd-resources.yaml  s3://aws-managed-cost-intelligence-dashboards/cfn/cid-crcd-resources.yaml
+aws s3 cp ./cloudformation/cid-crcd-resources.yaml  s3://aws-managed-cost-intelligence-dashboards/cfn/cid-crcd-resources.yaml
+aws s3 cp ./cirt/crcd-cirt-conformance-pack.yaml    s3://aws-managed-cost-intelligence-dashboards/cfn/crcd-cirt-conformance-pack.yaml
