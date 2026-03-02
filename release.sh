c4c5616a145759580767c0112123800f0ddda55a
Luca Casarini <llucc@amazon.com>
2026-03-02 16:53:03 +0100

    feature rename

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-aws s3 cp ./cloudformation/cid-crcd-resources.yaml  s3://aws-managed-cost-intelligence-dashboards/cfn/cid-crcd-resources.yaml
-aws s3 cp ./cirt/crcd-cirt-conformance-pack.yaml    s3://aws-managed-cost-intelligence-dashboards/cfn/crcd-cirt-conformance-pack.yaml
+aws s3 cp ./cloudformation/cid-crcd-resources.yaml s3://aws-managed-cost-intelligence-dashboards/cfn/cid-crcd-resources.yaml
+aws s3 cp ./crcd_conformance_pack/crcd-conformance-pack-resources.yaml s3://aws-managed-cost-intelligence-dashboards/cfn/crcd-conformance-pack.yaml
