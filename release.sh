8f22b2299c78fd32fb1a467a23185e1dfbb4d794
Luca Casarini <llucc@amazon.com>
2026-08-29 13:58:50 +0200

    bew release script

--- release.sh
+++ release.sh
@@ -4,3 +4,4 @@ aws s3 cp ./cloudformation/cid-crcd-stack.yaml s3://aws-managed-cost-intelligenc
 # aws s3 cp ./cloudformation/cid-crcd-resources.yaml s3://aws-managed-cost-intelligence-dashboards/cfn/cid-crcd-resources.yaml
 # aws s3 cp ./crcd_conformance_pack/crcd-conformance-pack-stack.yaml s3://aws-managed-cost-intelligence-dashboards/cfn/crcd-conformance-pack-stack.yaml
 aws s3 cp  ./backfill/crcd-backfill-resources.yaml   s3://aws-managed-cost-intelligence-dashboards/cfn/crcd-backfill-resources.yaml
+aws s3 cp ./preprocessing/crcd-config-preprocessing.yaml s3://aws-managed-cost-intelligence-dashboards/cfn/crcd-config-preprocessing.yaml
